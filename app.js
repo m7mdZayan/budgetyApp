a73aaed7ffdb6d01ed76e62aa5523c17edff6089
Mohamed Zayan <m7mdzayan@gmail.com>
2019-12-10 20:41:19 +0200

    items now can be added to our data structure

--- app.js
+++ app.js
@@ -1,7 +1,55 @@
-//budget controller
-let budgetController = (function() {})();
+//budget controller  /// first module
+let budgetController = (function() {
+  let Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
 
-//UI controller
+  let Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  let data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
+
+  return {
+    addItem: function(type, des, val) {
+      let newItem, ID;
+
+      // create new id
+      data.allItems[type].length > 0
+        ? (ID = data.allItems[type][data.allItems[type].length - 1].id + 1)
+        : (ID = 0);
+
+      //create new item based on whether type is inc or exp
+      type === "inc"
+        ? (newItem = new Income(ID, des, val))
+        : (newItem = new Expense(ID, des, val));
+
+      /// push it to our data structure
+      data.allItems[type].push(newItem);
+
+      //return the new element
+      return newItem;
+    },
+    testing: function() {
+      console.log(data);
+    }
+  };
+})();
+
+//UI controller  //// second module
 /**************************** */
 let uiController = (function() {
   let domStrings = {
@@ -24,7 +72,7 @@ let uiController = (function() {
 })();
 
 /*******************************************/
-// Global app controller
+// Global app controller  /// third module
 let controller = (function(budgetCtrl, uiCtrl) {
   let setupEventListeners = function() {
     let domStrings = uiCtrl.domStrings;
@@ -39,13 +87,20 @@ let controller = (function(budgetCtrl, uiCtrl) {
   };
 
   ctrlAddItem = function() {
-    let input = uiCtrl.getData();
+    let input, newItem;
+    // 1/ get the field input data
+    input = uiCtrl.getData();
     console.log(input);
+
+    // 2/add the item to the budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+    budgetCtrl.testing();
   };
 
   return {
+    /// initialization function
     init: function() {
-      console.log("starts now");
+      console.log("app starts now");
       setupEventListeners();
     }
   };
